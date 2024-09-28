bc0e4d67c7f934f026a7423148a694405bb6a3a6
Everton Soares <everton_dev@hotmail.com>
2024-09-28 18:44:11 -0300

    pokemons

---
+++ Apache Airflow/dags/dag_com_operador_python_parametros.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner' : 'Everton Soares',
+    'retries' : 5,
+    'retry_delay' : timedelta(minutes=5)
+}
+
+# função python q será chamada dentro da DAG no operator (python_callable)
+def greet(name, age):
+    print(f"Olá, Mundo! Meu nome é {name} e eu tenho {age} anos")
+
+with DAG(
+    dag_id = 'DAG_With_Python_Operator_Parameters',
+    description = 'Dag com operador Python',
+    start_date = datetime(2024, 9, 20),
+    schedule_interval = '@daily',
+    default_args = default_args
+) as dag :
+    # op_kwargs está passando os valores via parametro para a função greet()
+    task1 = PythonOperator(task_id='ola_mundo', python_callable=greet, op_kwargs={'name': 'Everton', 'age' : 26})
+    
+    task1
